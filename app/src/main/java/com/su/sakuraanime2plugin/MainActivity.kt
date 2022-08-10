cc75c2c67e16eb8ab4c9cea71929835ee3c8b947
RyensX <ryensu@qq.com>
2022-08-10 13:28:48 +0800

    [optimize]去除多余依赖减小体积

--- app/src/main/java/com/su/sakuraanime2plugin/MainActivity.kt
+++ app/src/main/java/com/su/sakuraanime2plugin/MainActivity.kt
@@ -1,27 +1,15 @@
 package com.su.sakuraanime2plugin
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
-import android.view.Menu
-import com.su.sakuraanime2plugin.databinding.ActivityMainBinding
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.pluginInfo.text = "这是樱花动漫MediaBox插件的主界面"
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menu?.add("关于")?.apply {
-            intent = Intent(this@MainActivity, OtherActivity::class.java)
-        }
-        return true
+        setContentView(TextView(this).apply {
+            text = "这是樱花动漫MediaBox插件的主界面"
+        })
     }
 }
