51f71a6ed47fceecdabccaa5be2beb3f7da9ecfd
RyensX <ryensu@qq.com>
2022-08-20 13:43:20 +0800

    Release 1.3(5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.su.sakuraanime2plugin"
         minSdk = 21
         targetSdk = 31
-        versionCode = 4
-        versionName = "1.2"
+        versionCode = 5
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
